11c4ca89bb74c539b5fc13479627aec0b76948fe
nurasyl005 <alimbeknurasyl@MacBook-Air-Alimbek.local>
2024-12-24 21:57:12 +0500

    READY

--- Desktop/alimbek python/alimbek python.py
+++ Desktop/alimbek python/alimbek python.py
@@ -1,1 +1,15 @@
-print ("Hello me friend")
+number = int(input("Enter a number: "))
+if number >= 7:
+    print("Hello")
+
+name = input("Enter a name: ")
+if name == "John":
+    print("Hello, John")
+else:
+    print("There is no such name")
+
+array = list(input("Enter a numeric array (comma-separated): ").split(' '))
+print("multiples of a 3:")
+for i in array:
+    if i % 3 == 0:
+        print(i)
